67e836e1824aec1a6283295241005a4804be495c
rileydollar8@gmail.com <ryan.anzaye@student.moringaschool.com>
2024-04-04 16:50:02 +0300

    modifying

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const testVar = "hi"
+
+document.queryselector("main#main").remove()
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "Ryan is the champion!"
+newHeader.className = "victory"
